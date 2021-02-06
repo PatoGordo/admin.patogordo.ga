7cf48ebd609d9e0a0a64f88da8d09d8061419daf
Pato Gordo <icariusv@gmail.com>
2021-02-06 19:27:06 +0000

    v1.0

--- Src/Pages/addpost.vue.js
+++ Src/Pages/addpost.vue.js
@@ -1,15 +1,23 @@
 Vue.component('AddPost', {
   data: function () {
     return {
-
+			
     }		
   },
 	methods:{
 		
 	},
+	mounted(){
+    ClassicEditor
+		.create( document.querySelector( '#editor' ) )
+		.catch( error => {
+			console.error( error );
+		});
+	},
   template: `
 	<div class="div-component addpost">
 		AddPost
+		<div id="editor"></div>
 	</div>
 	`
 })
--- Src/Pages/delete.vue.js
+++ Src/Pages/delete.vue.js
@@ -2,27 +2,39 @@ Vue.component('Delete', {
   data: function () {
     return {
 			contacts: [],
-			selected: '',
-			selectedEmail: ''
+			selected: ''
     }
   },
 	methods: {
-		
+		deleteData(){
+			contactRef.doc(this.selected).delete()
+			.then(() => {
+				this.selected = ''
+			})
+		}
 	},
 	mounted(){
-		// const items = [];
-		// contactRef.get()
-		// .then(res => {
-		// 	res.forEach(function(doc) {
-    //     //console.log(/* doc.id,*/ doc.data())
-		// 		items.push({ item : doc.data() })
-    // 	})
-		// 	this.contacts = items
-		// })
+		const items = [];
+		this.contacts = []
+		contactRef.get()
+		.then(res => {
+			res.forEach(function(doc) {
+				//console.log(/* doc.id,*/ doc.data())
+				items.push({ item : doc.id })
+			})
+			this.contacts = items
+		})
 	},
   template: `
 	<div class="div-component delete">
-		Delete
+		{{selected}}
+		<form @submit.prevent="deleteData()" class="form">
+			<h2 style="margin: 0 15px 10px 15px;">Delete Items</h2>
+			<select v-if="contacts && contacts.length" v-model="selected" class="input-box">
+				<option v-for="contact in contacts">{{contact.item}}</option>
+			</select>
+			<button type="submit" class="form-submit">Delete</button>
+		</form>
 	</div>`
 })
 
@@ -33,14 +45,3 @@ const Delete = {
 		<Delete />
 	`
 }
-
-/*<h1>Contacts</h1>
-		{{selectedEmail}}
-		<form @submit.prevent="deleteData()">
-			<h2>Select the item you want to delete.</h2>
-			<select v-if="contacts && contacts.length" v-model="selected" >
-				<option :key="contact.key" v-for="contact in contacts">{{contact.item.name}} {{contact.item.id}} </option>
-			</select>
-			<button type="submit">Delete</button>
-		</form>
-		*/
